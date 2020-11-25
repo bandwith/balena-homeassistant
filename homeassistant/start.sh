d15ed66a0b82925ebbe844735fc12325b498aea3
bandwith <62576+bandwith@users.noreply.github.com>
2020-11-24 23:09:47 -0500

    revert to homeassistant

--- homeassistant/start.sh
+++ homeassistant/start.sh
@@ -8,5 +8,10 @@ chmod +x /config/sensors.sh
 chmod +x /config/lovelace.sh
 
 if [ ! -f /config/ui-lovelace.yaml ]; then cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml; fi 
-
+if [ ! -d /config/custom_components/hacs ];
+  then apk add wget unzip;
+    wget https://github.com/hacs/integration/releases/latest/download/hacs.zip /tmp/hacs.zip;
+    unzip /tmp/hacs.zip -d /config/custom_components/hacs;
+    rm /tmp/hacs.zip;
+fi
 python -m homeassistant --config /config
